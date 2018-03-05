0033ea47505d5f651f988c19e673066ded0aec55
Bakhuss <ali.da@mail.ru>
2018-03-05 10:40:30 +0300

    commit

--- src/main/java/ru/bellintegrator/practice/office/model/Office.java
+++ src/main/java/ru/bellintegrator/practice/office/model/Office.java
@@ -1,19 +1,96 @@
 package ru.bellintegrator.practice.office.model;
 
+import javax.persistence.*;
+
+// Офис
+
+@Entity
 public class Office {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Version
+    private Integer version;
+
+    @Column
     private String name;
 
+    @Column(name = "organization_id")
     private Integer organizationId;
 
+    @Column
     private String address;
 
+    @Column
     private Integer phone;
 
+    @Column(name = "is_active")
     private Boolean isActive;
 
 
+    // constructors
 
+    public Office() {
+    }
+
+    // getters and setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(Integer organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
--- src/main/java/ru/bellintegrator/practice/user/model/User.java
+++ src/main/java/ru/bellintegrator/practice/user/model/User.java
@@ -1,21 +1,117 @@
 package ru.bellintegrator.practice.user.model;
 
+import javax.persistence.*;
+
+// Сотрудник
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Version
+    private Integer version;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
+    @Column(name = "second_name", nullable = false)
     private String secondName;
 
+    @Column(name = "middle_name")
     private String middleName;
 
+    @Column(name = "office_position_id")
     private Integer officePositionId;
 
+    @Column
     private Integer phone;
 
+    @Column(name = "citizenship_code")
     private Integer citizenshipCode;
 
+    @Column(name = "is_identified", nullable = false)
     private Boolean isIdentified;
 
+    // constructors
+
+    public User() {
+    }
+
+    // getters and setters
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Integer getOfficePositionId() {
+        return officePositionId;
+    }
+
+    public void setOfficePositionId(Integer officePositionId) {
+        this.officePositionId = officePositionId;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Integer getCitizenshipCode() {
+        return citizenshipCode;
+    }
+
+    public void setCitizenshipCode(Integer citizenshipCode) {
+        this.citizenshipCode = citizenshipCode;
+    }
+
+    public Boolean getIdentified() {
+        return isIdentified;
+    }
+
+    public void setIdentified(Boolean identified) {
+        isIdentified = identified;
+    }
 }
